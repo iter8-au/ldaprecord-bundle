771c09a13c0981e5757b56f499173b36bd2ae47d
Dan Barrett <dan@danbarrett.com.au>
2021-01-04 13:16:16 +1100

    Minor fixes

--- DependencyInjection/Iter8LdapRecordExtension.php
+++ DependencyInjection/Iter8LdapRecordExtension.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Iter8\Bundle\LdapRecordBundle\DependencyInjection;
 
 use InvalidArgumentException;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
 /**
@@ -15,6 +17,9 @@ class Iter8LdapRecordExtension extends ConfigurableExtension
 {
     protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
     {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+
         if ($mergedConfig['use_ssl'] && $mergedConfig['use_tls']) {
             throw new InvalidArgumentException('Cannot configure AD/LDAP connection to use both TLS and SSL, please pick one.');
         }
